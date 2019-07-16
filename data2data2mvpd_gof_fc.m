7b26c0ca8815978d8c6f95db460eb05f5dcedb4c
alessiobasti <40668571+alessiobasti@users.noreply.github.com>
2019-07-16 10:12:42 +0200

    Update and rename data2mvpd.m to data2data2mvpd_gof_fc.m

--- data2data2mvpd_gof_fc.m
+++ data2data2mvpd_gof_fc.m
@@ -1,19 +1,24 @@
-function [mvpd,fc,fc_pc]=data2mvpd(Ya,Yb,options);
+function [mvpd,gof,fc,fc_pc]=data2mvpd_gof_fc(Ya,Yb,options);
 % calculates the MultiVariate Pattern Dependence (MVPD) between two
-% multivariate time series (Anzellotti et al. 2017, Plos Comput Biol).
+% multivariate time series (Anzellotti et al. 2017, Plos Comput Biol), the 
+% goodness-of-fit (GOF) metric (Basti et al. 2019), the Pearson correlation
+% between the average time series and the one between the first two
+% principal components (PCs).
 %
 % Input:
-% Ya and Yb:  two cell arrays. The number of cells represents the number of runs, and the dimension of each cell is 
-%             equal to ntxna, for the ROIa, and ntxnb, for the ROIb.
-% options     method: either pca_exvar or pca_ndir; either percentage or number: the percentage of variance that we want to explain by
-%             applying dimensionality reduction approach or the number of
-%             ICs to be considered.
+% Ya and Yb:  two cell arrays. The number of cells represents the number of runs, 
+%             and the dimension of each cell is equal to ntxna, for the ROIa,
+%             and ntxnb, for the ROIb.
+% options     method: either pca_exvar or pca_ndir; either percentage or number:  
+%             the percentage of variance that we want to explain by applying 
+%             dimensionality reduction approach or the number ofICs to be considered.            
 % Output: 
 % mvpd:       MVPD value.
-% uvpd:       UVPD value.
-% fc:         Pearson correlation coefficient.
+% gof:        GOF value.
+% fc:         Pearson correlation coefficient between average time series.
+% fc:         Pearson correlation coefficient between first two PCs.
 % Alessio Basti 
-% version: 04/07/2019
+% version: 16/07/2019
 
 for irun=1:length(Ya)
     %Ya_zs{irun} = zscore(Ya{irun},0,2);
@@ -25,13 +30,13 @@ for irun=1:length(Ya)
     ts_b{irun} = mean(Yb{irun},2);
     % compute the pearson correlation
     fc_app(irun) = corr(ts_a{irun},ts_b{irun});
-    [PC1_a{irun}]=dimreduction(Ya_zs{irun},'pca_ndir',options);
-    [PC1_b{irun}]=dimreduction(Yb_zs{irun},'pca_ndir',options);
+    [PC1_a{irun}]=dimreduction(Ya{irun},'pca_ndir',options);
+    [PC1_b{irun}]=dimreduction(Yb{irun},'pca_ndir',options);
     fc_PCs_app(irun) = corr(PC1_a{irun},PC1_b{irun});
 end
 
 method=zeros(2,1);
-for jmet=1:2
+for jmet=1:1
     if(jmet==1)
        Ya_app=Ya_zs; 
        Yb_app=Yb_zs;
@@ -56,23 +61,24 @@ for jmet=1:2
         [Yatrain_red,Va,SVa]=dimreduction(Yatrain,options.method,options);
         [Ybtrain_red,Vb,SVb]=dimreduction(Ybtrain,options.method,options);
 
-        %linear model estimate (least-square method)
-        B=Ybtrain_red'*Yatrain_red*pinv(Yatrain_red'*Yatrain_red);
+        %linear model estimate (ridge regression) and gof
+        [B,~]=ridgeregmethod(Yatrain_red,Ybtrain_red,options.regularisation);
         Yatest_red=(Yatest-repmat(mean(Yatest),length(Yatest(:,1)),1))*Va;
         Ybtest_red=(Ybtest-repmat(mean(Ybtest),length(Ybtest(:,1)),1))*Vb;
-
+        
         % correlation between the forecasted and the test data
         Ybtest_for_red=Yatest_red*B';
         for icomp=1:length(Ybtest_for_red(1,:))
             M=corrcoef(Ybtest_red(:,icomp),Ybtest_for_red(:,icomp));
             method(jmet)=method(jmet)+(SVb(icomp)/sum(SVb))*M(1,2);
         end
+        [B,gof(irun,jmet)]=ridgeregmethod(Ya_zs{irun},Yb_zs{irun},options.regularisation);
     end
 end
 
 %averaging across the runs the MVPD (method(1)) and UVPD (method(2))
 mvpd=method(1)/length(Ya_app);
-uvpd=method(2)/length(Ya_app);
+gof=mean(gof(:,1));
 fc=mean(fc_app);
 fc_pc=mean(fc_PCs_app);
 
