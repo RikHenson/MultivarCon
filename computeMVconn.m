1412c3dfbce99853a2be13e810b8f83fe9c9280e
alessiobasti <40668571+alessiobasti@users.noreply.github.com>
2019-07-29 10:52:51 +0200

    Update computeMVconn.m

--- computeMVconn.m
+++ computeMVconn.m
@@ -1,4 +1,4 @@
-function [MVconn,MVconn_null] = computeMVconn(Ya,Yb,opt)
+function [MVconn,MVconn_null] = computeMVconn(X,Y,opt)
 
 if ~isfield(opt,'nRandomisation')
     opt.nRandomisation = 1;
@@ -6,37 +6,37 @@ end
 
 
 % Calculate connectivity on data given
-for s=1:length(Ya)
+for s=1:length(X)
     if ~isfield(opt,'segleng') 
-        [mvpd(s,1),gof(s,1),fc(s,1),fc_pc(s,1)] = data2mvpd_gof_fc(Ya{s},Yb{s},opt); 
-        [dcor(s,1),dcor_u(s,1)] = data2dCor(Ya{s},Yb{s});
-        [rc(s,1),~] = data2rc(Ya{s},Yb{s},'Correlation');
+        [mvpd(s,1),gof(s,1),fc(s,1),fc_pc(s,1)] = data2mvpd_gof_fc(X{s},Y{s},opt); 
+        [dcor(s,1),dcor_u(s,1)] = data2dCor(X{s},Y{s});
+        [rc(s,1),~] = data2rc(X{s},Y{s},'Correlation');
     else
-       [mim(s,1),imcoh(s,1),imcoh_pc(s,1)] = data2mim(Ya{s},Yb{s},opt);
+       [mim(s,1),imcoh(s,1),imcoh_pc(s,1)] = data2mim(X{s},Y{s},opt);
     end
 end
 
-% Calculate connectivity when Ya and Yb independent random noise (since
+% Calculate connectivity when X and Y independent random noise (since
 % some connectivity measures, eg Dcor, not bounded by 0 or -1)
-if length(Ya)<20
+if length(X)<20
     warning('Insufficient subjects (<20) to estimate baseline error')
 else
     for iter = 1:opt.nRandomisation
         if opt.nRandomisation > 1
             fprintf('iteration %d from %d \n',iter,opt.nRandomisation)
         end
-        for s=1:length(Ya) % Ensure reasonably accurate estimate
-            bYa = {}; bYb = {};
-            for r=1:length(Ya{s})
-                bYa{r} = Ya{s}{r}(randperm(size(Ya{s}{r},1)),:);
-                bYb{r} = Yb{s}{r}(randperm(size(Yb{s}{r},1)),:);
+        for s=1:length(X) % Ensure reasonably accurate estimate
+            bX = {}; bY = {};
+            for r=1:length(X{s})
+                bX{r} = X{s}{r}(randperm(size(X{s}{r},1)),:);
+                bY{r} = Y{s}{r}(randperm(size(Y{s}{r},1)),:);
             end
             if ~isfield(opt,'segleng') 
-                [bmvpd(s,iter),bgof(s,iter),bfc(s,iter),bfc_pc(s,iter)] = data2mvpd_gof_fc(bYa,bYb,opt);
-                [bdcor(s,iter),bdcor_u(s,iter)] = data2dCor(bYa,bYb);
-                [brc(s,iter),~] = data2rc(bYa,bYb,'Correlation');
+                [bmvpd(s,iter),bgof(s,iter),bfc(s,iter),bfc_pc(s,iter)] = data2mvpd_gof_fc(bX,bY,opt);
+                [bdcor(s,iter),bdcor_u(s,iter)] = data2dCor(bX,bY);
+                [brc(s,iter),~] = data2rc(bX,bY,'Correlation');
             else
-               [bmim(s,iter),bimcoh(s,iter),bimcoh_pc(s,iter)] = data2mim(bYa,bYb,opt);
+               [bmim(s,iter),bimcoh(s,iter),bimcoh_pc(s,iter)] = data2mim(bX,bY,opt);
             end
         end
     end
@@ -69,13 +69,3 @@ else
 end
 
 return
-
-
-
-
-
-
-
-
-
-
