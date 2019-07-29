633ca2c49da83a42e18e14140b9ed08b1f114cef
alessiobasti <40668571+alessiobasti@users.noreply.github.com>
2019-07-29 10:55:49 +0200

    Update data2rc.m

--- data2rc.m
+++ data2rc.m
@@ -1,4 +1,4 @@
-function [rc_w,rc_b] = data2rc(Ya,Yb,metric,corrType);
+function [rc_w,rc_b] = data2rc(X,Y,metric,corrType);
 % This function computes the representational connectivity from the
 % multivariate time series of two regions. 
 % It computes both the wihtin- and between-run RDM correlations for the two
@@ -12,11 +12,11 @@ if ~exist('corrType','var')
     corrType = 'Pearson';
 end
 
-nruns = numel(Ya); % number of runs
+nruns = numel(X); % number of runs
 
 for r=1:nruns
-    rdms1(:,r)    = pdist(Ya{r},metric)';
-    rdms2(:,r)    = pdist(Yb{r},metric)';
+    rdms1(:,r)    = pdist(X{r},metric)';
+    rdms2(:,r)    = pdist(Y{r},metric)';
 end
 corrmat  = corr(rdms1,rdms2,'type',corrType);
 rc_w = mean(diag(corrmat));
