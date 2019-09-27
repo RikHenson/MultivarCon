ca643e234178daf01c9e205a9f68103aea03f75b
alessiobasti <40668571+alessiobasti@users.noreply.github.com>
2019-09-27 13:22:07 +0200

    Update demo.m

--- demo.m
+++ demo.m
@@ -15,7 +15,7 @@ opt.method = 'svd_exvar'; % reduce dimensions based on percent variance
 opt.percentage = 90; % percent variance cut-off
 opt.meancorrection = 1; % for dimension reduction (overwritten for pca_fc in data2mvpd_lprd_fc.m)
 opt.regularisation = 10.^(-1:0.2:3); % regularisation parameter for ridge regression.
-opt.nRandomisation = 1;
+opt.nRandomisation = 20;
 
 rng('default')
 
