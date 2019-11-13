abb709c46b60c83e6ff4dc7dd75e659eb626843a
RikHenson <rik.henson@mrc-cbu.cam.ac.uk>
2019-11-13 09:41:45 +0000

    Small changes
    Found 2D example where SVD doesn't work but RCA does.
    Corrected Demo 3 and commented out Demos 8 and 9 because too different to others

--- Example2D.m
+++ Example2D.m
@@ -1,20 +1,22 @@
 
 cd /home/rh01/GitHub/MultivarCon
+options.number = 1;
+options.meancorrection = 0;
 
 %% 1 When Pearson of Mean works
-d = 0.2;
 
 X = [
-    -3  2
-    -2  2
-    3 -2
-    2 -2
+    -3  -2
+    -2  -3
+     3   2
+     2   3
     ];
 
-%X = X + d*[0 -1; 0 1; 0 1; 0 -1]; %  + 0.1*randn(4,2);               
-Y = X; % + 0.1*randn(4,2);       
-
+Y = X; 
+X1=dimreduction(X,'svd_ndir',options);
+Y1=dimreduction(Y,'svd_ndir',options);
 corr(mean(X,2),mean(Y,2))
+corr(X1,Y1) % X1,Y1 projections along y=x
 
 figure, 
 subplot(1,2,1); title('ROI1')
@@ -27,23 +29,14 @@ print -dpng 'UVmean.png'
 
 
 %% When Pearson of SVD works
-X = [
-    -3   2
-    -1   2
-    -3   1
-    -1   1
-    ];  
-Y = [
-    1   -2
-    3   -2
-    1   -1
-    3   -1
-    ];  
-%X = X + d*[-1 1; 1 -1; 1 -1; -1 1]; % + 0.1*randn(4,2);         
-%Y = [-X(:,1) X(:,2)];
-%Y = Y + d*[-1 -1; 1 1; 1 1; -1 -1]; % + 0.1*randn(4,2);          
+    
+Y = [-X(:,1) X(:,2)];
+
 corr(mean(X,2),mean(Y,2))
-corr(diff(X,1,2),sum(Y,2)) %equivalent to SVD, ie projecting along y=-x
+X1=dimreduction(X,'svd_ndir',options);
+Y1=dimreduction(Y,'svd_ndir',options);
+corr(X1,Y1)
+%corr(diff(X,1,2),sum(Y,2)) % equivalent to SVD, ie projecting along y=-x
 
 figure, 
 subplot(1,2,1); title('ROI1')
@@ -55,29 +48,31 @@ axis([-4 4 -4 4]), set(gca,'XAxisLocation','origin','YAxisLocation','origin','XT
 print -dpng 'UVsvd.png'
 
 
-%% When MV needed, because data not dominated by one direction (though SVD=diff also works in this case?)
-
-d = 1;
-
+%% When MV needed, because data not dominated by one direction 
+    
 X = [
-    2   2
-    2   2
-    -2  -2
-    -2  -2
-    ];    
-X = X + d*[-1 1; 1 -1; 1 -1; -1 1];% + 0.1*randn(4,2);             
-Y = [-X(:,1) X(:,2)];
-Y = Y + d*[-1 -1; 1 1; 1 1; -1 -1]; % + 0.1*randn(4,2);               
+     0 -3
+    -3  0
+     1  1
+     2  2
+    ];
+
+Y = [
+     3  -3
+    -3   3
+    -1/2 0
+     0 1/2
+];
+
 corr(mean(X,2),mean(Y,2))
-corr(diff(X,1,2),sum(Y,2)) % Still -1...
+X1=dimreduction(X,'svd_ndir',options);
+Y1=dimreduction(Y,'svd_ndir',options);
+corr(X1,Y1) 
+%corr(sum(X,2),diff(Y,1,2))  % equivalent to SVD, ie projecting along y=-x
 
 DX = pdist(X,'euclidean');
 DY = pdist(Y,'euclidean');
 corr(DX',DY')
-
-figure,
-subplot(1,2,1); title('ROI1'), imagesc(squareform(DX)), colormap('gray'), axis off
-subplot(1,2,2); title('ROI2'), imagesc(squareform(DY)), colormap('gray'), axis off
 
 figure, 
 subplot(1,2,1); title('ROI1')
@@ -87,5 +82,10 @@ subplot(1,2,2); title('ROI2')
 hold on, for t=1:size(X,1); t = text(Y(t,1),Y(t,2),num2str(t),'FontSize',12); end; axis square
 axis([-4 4 -4 4]), set(gca,'XAxisLocation','origin','YAxisLocation','origin','XTick',[],'YTick',[]), 
 print -dpng 'MV1.png'
+
+% RDM representation
+figure,
+subplot(1,2,1); imagesc(squareform(DX)), colormap('gray'), set(gca,'XTick',[1:4]), set(gca,'YTick',[1:4]), axis square, title('ROI1') 
+subplot(1,2,2); imagesc(squareform(DY)), colormap('gray'), set(gca,'XTick',[1:4]), set(gca,'YTick',[1:4]), axis square, title('ROI2') 
 
 
