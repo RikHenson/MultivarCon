81b18c53e9cc70ce31319eb48a2e4e859db45212
alessiobasti <40668571+alessiobasti@users.noreply.github.com>
2020-05-02 09:51:24 +0200

    Update plotmv.m

--- plotmv.m
+++ plotmv.m
@@ -92,8 +92,8 @@ end
 % plot the absolute performance
 if ~isfield(MVconn,'MIM')
     subplot(3,2,5), hold on
-    c = categorical({'1 Pearson','2 Pearson-SVD','3 MVPD','4 dCor','5 Pearson-RCA','6 LPRD'});
-    allval = [MVconn.FC MVconn.FCSVD MVconn.MVPD MVconn.dCor MVconn.RCA MVconn.LPRD];
+    c = categorical({'1 Pearson','2 Pearson-SVD','3 Pearson-CCA','4 MVPD', '5 dCor','6 Pearson-RCA','7 LPRD'});
+    allval = [MVconn.FC MVconn.FCSVD MVconn.FCCCA MVconn.MVPD MVconn.dCor MVconn.RCA MVconn.LPRD];
     meanvl = mean(allval);
     spread = std(allval);
     % spread = iqr(allval);
@@ -132,7 +132,7 @@ if length(X)<20
 else
     if ~isfield(MVconn,'MIM')
         subplot(3,2,6), hold on
-        allnul = [MVconn_null.FC MVconn_null.FCSVD MVconn_null.MVPD MVconn_null.dCor MVconn_null.RCA MVconn_null.LPRD];
+        allnul = [MVconn_null.FC MVconn_null.FCSVD MVconn_null.FCCCA MVconn_null.MVPD MVconn_null.dCor MVconn_null.RCA MVconn_null.LPRD];
         % meanvl = meanvl - mean(allnul);
         % spread = sqrt(spread.^2 + var(allnul));
         meanvl = mean(allval - allnul);
